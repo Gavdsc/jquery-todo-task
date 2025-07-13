4717fe0a1e32758e657f013e43b7596cbfea5954
GSC <gav@gavsblog.com>
2025-07-13 17:24:02 +0100

    Adding pagination class for modularity/separation of concerns.
    Using bootstrap default navigation styling to match design.
    
    Added pagination updates to table rendering.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     // Just check the JQuery CDN is loading
     console.log('JQuery loaded'); 
     
-    new TodoApp('https://jsonplaceholder.typicode.com/todos', 'todoList', 'search', 'warningBox')
+    new TodoApp('https://jsonplaceholder.typicode.com/todos', 'todoList', 'search', 'todoPagination', 'warningBox')
 });
 
 /**
@@ -20,13 +20,22 @@ class TodoApp {
         column: 'id',
         ascending: true
     }
-    
-    constructor(apiEndpoint, tableId, searchId, warningId) {
+
+    /**
+     * Constructor
+     * @param {string} apiEndpoint
+     * @param {string} tableId
+     * @param {string} paginationId
+     * @param {string} searchId
+     * @param {string} warningId
+     */
+    constructor(apiEndpoint, tableId, searchId, paginationId, warningId) {
         this.apiEndpoint = apiEndpoint;
 
         // Select once, and save
         this.table = $(`#${tableId}`);
         this.search = $(`#${searchId}`);
+        this.pagination = new Pagination($(`#${paginationId}`), this.onPageChange);
         this.warning = $(`#${warningId}`);
         
         // Clear the search on page reload
@@ -63,6 +72,9 @@ class TodoApp {
                 // Remember to filter and sort on success (before render)
                 // This will allow url params to be collected for scalability
                 this.filterAndSort();
+                
+                // Update the pagination after we have data
+                this.pagination.update(this.filteredTodos.length, 1);
 
                 // Render the table
                 this.renderTable();
@@ -77,8 +89,15 @@ class TodoApp {
      * Render the table.
      */
     renderTable() {
+        // Find the start of the list and end of the list based on the current page (adjusted -1)
+        const start = (this.pagination.current - 1) * this.pagination.perPage;
+        const end = start + this.pagination.perPage;
+        
+        // Slice the list for looping
+        const paginatedTodos = this.filteredTodos.slice(start, end);
+        
         // Map the rows
-        const rows = this.filteredTodos.map(todo => `
+        const rows = paginatedTodos.map(todo => `
             <tr>
                 <td>${todo.id}</td> 
                 <td>${todo.title}</td>   
@@ -100,6 +119,9 @@ class TodoApp {
             
             // Filter
             this.filterAndSort();
+
+            // Reset the pagination to 1 (or we risk rendering an empty table)
+            this.pagination.update(this.filteredTodos.length, 1);
             
             // Update the render
             this.renderTable();
@@ -110,7 +132,7 @@ class TodoApp {
                 return;
             }
             
-            // Otherwise remove existing warnings
+            // Otherwise, remove existing warnings
             // Note: might need to re-think this if I have more errors to throw later
             if (this.warningState)
                 this.onError("");
@@ -169,6 +191,16 @@ class TodoApp {
     updateSortIcons(target) {
         $('.active-asc, .active-dsc').removeClass(['active-asc', 'active-dsc']);
         target.classList.add(this.sortState.ascending ? 'active-asc' : 'active-dsc');
+    }
+
+    /**
+     * Page change callback for pagination clicks.
+     * Arrow function so scope is 'this' object, not the pagination.
+     * @param {number} page - The page number selected by the user
+     */
+    onPageChange = (page) => {
+        this.pagination.update(this.filteredTodos.length, page);
+        this.renderTable();
     }
 
     /**
@@ -233,6 +265,153 @@ class TodoSort {
 
             return 0;
         });
+    }
+}
+
+/**
+ * Class to generate pagination and hold pagination state.
+ * @class Pagination
+ */
+class Pagination {
+    
+    // Simple state
+    // Todo: pass these in constructor to make reusable
+    current = 1;
+    perPage = 10;
+    todoCount = 0;
+    visible = 5;
+    
+    /**
+     * Constructor
+     * @param {JQuery<HTMLElement>} container
+     * @param {function} onPageChange
+     * @param {number} visible
+     */
+    constructor(container, onPageChange, visible = 5) {
+        this.container = container;
+        this.onPageChange = onPageChange;
+        this.visible = visible;
+
+        this.bind();
+    }
+
+    /**
+     * Set up event delegation using jQuery
+     */
+    bind() {
+        // Use event delegation
+        this.container.on('click', '.page-link', (event) => {
+            event.preventDefault();
+
+            // Coerce here too, just to be safe
+            const page = +(event.currentTarget.dataset.page)
+
+            // Prefer defensive coding
+            if (isNaN(page))
+                return;
+
+            // Fire callback
+            this.onPageChange(page)
+        });
+    }
+
+    /**
+     * Render pagination.
+     * Note: Ideally break up this render function into something smaller.
+     */
+    render() {
+        // First get the number of pages (filteredTodos, not todos)
+        const pages = Math.ceil(this.todoCount / this.perPage);
+
+        // First empty the pagination
+        this.container.empty();
+
+        // Don't draw pagination if only 1 page
+        if (pages === 1)
+            return;
+
+        // Set previous and next pages 
+        const previous = this.current > 1 ? this.current - 1 : 1;
+        const next = this.current < pages ? this.current + 1 : pages;
+
+        // First button
+        this.addButton(1, "First", this.current === 1);
+
+        // Previous button
+        this.addButton(previous, "Previous", this.current === 1);
+
+        // Design only renders 5
+        // Render previous 2, current and next 2
+        const { start, end } = this.getVisiblePagination(pages)
+
+        // Page buttons (only 5)
+        for (let i = start; i <= end; i++) {
+            this.addButton(i, i);
+        }
+
+        // Next button
+        this.addButton(next, "Next", this.current === pages);
+
+        // Last button
+        this.addButton(pages, "Last", this.current === pages);
+    }
+
+    /**
+     * Filter for start and end based on current page.
+     * @param {number} pages
+     * @returns {{start: number, end: number}}
+     */
+    getVisiblePagination(pages) {
+        // Guard against less pages than total
+        if (pages <= this.visible)
+            return { start: 1, end: pages }
+
+        // First, we need half the max visible from the state
+        const halfVisible = Math.floor(this.visible / 2);
+
+        // The start is whatever is greater, 1 or the current - half the max visible (render half before the current, unless less than half visible)
+        let start = Math.max(1, this.current - halfVisible);
+
+        // Can just set the end to be the start + the max visible -1
+        let end = start + this.visible - 1;
+
+        // Adjust if end goes beyond total
+        if (end > pages) {
+            end = pages;
+            start = Math.max(1, end - this.visible + 1);
+        }
+
+        return { start, end }
+    }
+
+    /**
+     * Utility to add buttons to the pagination.
+     * @param {number} page
+     * @param {string} title
+     * @param {boolean} disabled
+     */
+    addButton(page, title, disabled = false) {
+        this.container.append(`
+            <li class="page-item ${title == this.current ? "active" : ""} ${disabled ? "disabled" : ""}">
+                <a class="page-link" href="#" aria-label="Go to page ${page}" data-page="${page}">${title}</a>
+            </li>
+        `);
+    }
+
+    /**
+     * Update pagination and re-render.
+     * @param {number} todoCount
+     * @param {number} current
+     */
+    update(todoCount, current) {
+        // Don't re-render if unnecessary 
+        if (this.todoCount === todoCount && this.current === current)
+            return;
+
+        this.todoCount = todoCount;
+        // Bug: No TypeScript, so don't forget to coerce to number first
+        this.current = +current;
+        this.render();
     }
 }
 
